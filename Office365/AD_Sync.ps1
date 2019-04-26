4482742a7661f07f015bba6268f4c9705ae0c136
Gabriel Vale <32900651+GabrielVale@users.noreply.github.com>
2019-04-26 16:04:18 -0300

    remote cmd
    incrementado para execução remota

--- Office365/AD_Sync.ps1
+++ Office365/AD_Sync.ps1
@@ -1,8 +1,7 @@
 
 # Command to exec full sync
-
-Invoke-Command -ComputerName RKDC01 -ScriptBlock {
-    Import-Module adsync
-    Start-ADSyncSyncCycle -PolicyType Initial
-    }
+$ADSyncServer = ''
+Invoke-Command -ComputerName $ADSyncServer -ScriptBlock {
+Import-Module adsync
+Start-ADSyncSyncCycle -PolicyType Initial}
 ########################################################
